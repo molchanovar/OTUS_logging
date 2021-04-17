b000e22fb8268138da5ab031148a98ba7e03b29b
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-04-17 15:23:08 +0300

    Create web.sh

---
+++ web.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+yum -y install vim 
+yum -y install epel-release 
+dnf -y install nginx
+yum -y install audispd-plugins
+
+cp /vagrant/web/nginx.conf /etc/nginx/nginx.conf
+cp /vagrant/web/rsyslog.conf /etc/rsyslog.conf
+cp /vagrant/web/auditd.conf /etc/audit/auditd.conf
+cp /vagrant/web/audit.rules /etc/audit/rules.d/audit.rules
+cp /vagrant/web/audisp-remote.conf /etc/audit/audisp-remote.conf
+cp /vagrant/web/au-remote.conf /etc/audit/plugins.d/au-remote.conf
+
+service auditd restart
+systemctl restart rsyslog
+systemctl restart nginx
